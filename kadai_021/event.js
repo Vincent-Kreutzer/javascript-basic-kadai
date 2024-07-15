f5898a50c042b7a4394c96c5cc07aec679224886
Vincent-Kreutzer <tsukishiro1986@hotmail.co.jp>
2024-07-15 18:21:50 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-btn = document.getElementById('btn');
-
-btn.addEventListener('click', ()=> {
-  text = document.getElementById('text');
-  setTimeout(()=> {
-    text.textContent = 'ボタンをクリックしました';
-}
-, 2000)});
